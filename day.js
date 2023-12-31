0000d632eafd81c4d2be3a1a460ef70b273dc441
ficzclay <ficzclay@gmail.com>
2023-12-31 09:03:23 +0700

    tambahkan data

---
+++ day.js
@@ -0,0 +1,5 @@
+if(day === "sabtu" && day === "minggu") {
+  console.log("Enjoy the weekend");
+} else {
+  console.log("Do some work!");
+}
---
+++ index.py
@@ -0,0 +1,7 @@
+import numpy as np
+ 
+#buat data jumlah kamar
+bedrooms = np.array([1,1,2,2,3,4,4,5,5,5])
+ 
+#data harga rumah. asumsi dalam dollar
+house_price = np.array([15000, 18000, 27000, 34000, 50000, 68000, 65000, 81000,85000, 90000])
